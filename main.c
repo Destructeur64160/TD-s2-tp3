92f3191eead98345f41ca93f99408a6dc61d0349
Cooper <kevinjallet64@gmail.com>
2026-02-25 15:26:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "puissance4.h"
+
+int N;
 
 int main() {
     printf("C'est le puissance 4 ! \n");
-    system("sleep 15 ");
+    N=7;
+    printf("valeur de N: %d\n", N);
+    int** grille;
+    grille = allouer(N);
+    initialiser(grille, N);
+    afficher(grille,N);
     return 0;
 }
