7508dd7787c76f820edab368f849cd1c938b566a
Cooper <kevinjallet64@gmail.com>
2026-02-25 15:26:49 +0100

    Update puissance4.c

--- puissance4.c
+++ puissance4.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "puissance4.h"
+
+/* same as allouer in puissance4.h */
+int** allouer(int taille){
+	int** grille;
+	grille=malloc(taille*sizeof(int*));
+	for (int i=0; i<taille; i++){
+		grille[i]=malloc(taille*sizeof(int));
+	}
+	return grille;
+}
+
+/* same as initialiser in puissance4.h*/
+void initialiser(int** maGrille, int taille){
+	for (int i=0; i<taille; i++){
+		for (int j=0; j<taille; j++){
+			maGrille[i][j]=-1;
+		}
+	}
+}
+
+/* same as afficher in puissance4.h */
+void afficher(int** maGrille, int taille){
+	for (int i=0; i<taille; i++){
+		for (int j=0; j<taille; j++){
+			printf("|");
+			if (maGrille[i][j]==-1){
+				printf("	");
+			}
+			else if (maGrille[i][j]==1){
+				printf(" X ");
+			}
+			else {
+				printf(" O ");
+			}
+		}
+		printf("|\n");
+	}
+}
+
+/* same as jouer in puissance4.h */
+void jouer(int** maGrille, int taille, int joueur){
+	
+}
