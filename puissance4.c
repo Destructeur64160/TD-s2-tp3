a415b4dda9c2cf7d85d061041b9fc552baea657f
Cooper <kevinjallet64@gmail.com>
2026-02-26 23:44:35 +0100

    Refactor functions to use 'grille' instead of 'maGrille'

--- puissance4.c
+++ puissance4.c
@@ -13,27 +13,27 @@ int** allouer(int taille){
 }
 
 /* same as initialiser in puissance4.h*/
-void initialiser(int** maGrille, int taille){
+void initialiser(int** grille, int taille){
 	for (int i=0; i<taille; i++){
 		for (int j=0; j<taille; j++){
-			maGrille[i][j]=-1;
+			grille[i][j]=-1;
 		}
 	}
 }
 
 /* same as afficher in puissance4.h */
-void afficher(int** maGrille, int taille){
+void afficher(int** grille, int taille){
 	for (int i=0; i<taille; i++){
 		for (int j=0; j<taille; j++){
 			printf("|");
-			if (maGrille[i][j]==-1){
+			if (grille[i][j]==-1){
 				printf("	");
 			}
-			else if (maGrille[i][j]==1){
-				printf(" X ");
+			else if (grille[i][j]==1){
+				printf("   X   ");
 			}
 			else {
-				printf(" O ");
+				printf("   O   ");
 			}
 		}
 		printf("|\n");
@@ -41,6 +41,42 @@ void afficher(int** maGrille, int taille){
 }
 
 /* same as jouer in puissance4.h */
-void jouer(int** maGrille, int taille, int joueur){
+void jouer(int** grille, int taille, int joueur){
+	int col;
+	int i=0;
+	int placement=0; //condition valant 1(True) ou 0(False) pour savoir si on peu placer le pion
+	int valide=0; //conditions pour verifier si le placement est valide;
+	printf("Où voulez-vous placez votre pion?");
+	scanf("%d", &col);
+	while(valide==0){
+		if (col>0 && col<taille-1 && grille[0][col]==-1){
+			while(placement==0){
+				if (grille[i][col] && i<taille-1){ // verifie si la case du dessous est libre et que l'on reste dans la matrice
+					placement=0;
+					i=i+1;
+				}
+				
+				else if(grille[i][col]==-1 && i==(taille-1)){
+					placement=1;
+					grille[i][col]=joueur;
+				}
+				else{
+					placement=1;
+					grille[i][col]=joueur;
+				}
+			}
+			valide=1;
+			placement=0;
+		}
+		else{
+			printf("Merci de bien vouloir saisir une colonne valide\n");
+			afficher(grille,taille);
+			scanf("%d",&col);
+		}
+	}
+}
+
+/* same as VerificationLignes in puissance4.h */
+int VerificationLignes(int** grille, int ligne, int joueur, int taille){
 	
 }
