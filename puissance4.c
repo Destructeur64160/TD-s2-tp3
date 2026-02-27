20aa71bfa7a66cd7dd2f7a85c04cacc8da2b08f5
Cooper <kevinjallet64@gmail.com>
2026-02-27 15:43:59 +0100

    Update puissance4.c

--- puissance4.c
+++ puissance4.c
@@ -27,13 +27,13 @@ void afficher(int** grille, int taille){
 		for (int j=0; j<taille; j++){
 			printf("|");
 			if (grille[i][j]==-1){
-				printf("	");
+				printf(" ");
 			}
 			else if (grille[i][j]==1){
-				printf("   X   ");
+				printf("X");
 			}
 			else {
-				printf("   O   ");
+				printf("O");
 			}
 		}
 		printf("|\n");
@@ -49,24 +49,24 @@ void jouer(int** grille, int taille, int joueur){
 	printf("Où voulez-vous placez votre pion?");
 	scanf("%d", &col);
 	while(valide==0){
-		if (col>0 && col<taille-1 && grille[0][col]==-1){
+		if (col>=0 && col<taille && grille[0][col]==-1){
 			while(placement==0){
-				if (grille[i][col] && i<taille-1){ // verifie si la case du dessous est libre et que l'on reste dans la matrice
+				if (grille[i][col]==-1 && i<taille-1){ // verifie si la case du dessous est libre et que l'on reste dans la matrice
 					placement=0;
 					i=i+1;
 				}
-				
 				else if(grille[i][col]==-1 && i==(taille-1)){
 					placement=1;
 					grille[i][col]=joueur;
 				}
-				else{
+				else if(grille[i][col]!=-1 && i>0){
 					placement=1;
-					grille[i][col]=joueur;
+					grille[i-1][col]=joueur;
 				}
 			}
 			valide=1;
 			placement=0;
+			i=0;
 		}
 		else{
 			printf("Merci de bien vouloir saisir une colonne valide\n");
@@ -78,5 +78,52 @@ void jouer(int** grille, int taille, int joueur){
 
 /* same as VerificationLignes in puissance4.h */
 int VerificationLignes(int** grille, int ligne, int joueur, int taille){
-	
+	for (int i=0; i<taille-3; i++){
+
+		if(grille[ligne][i] == grille[ligne][i+1] && grille[ligne][i+1] == grille[ligne][i+2] && grille[ligne][i+2] == grille[ligne][i+3] && grille[ligne][i]!=-1){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/* same as VerificationColonnes in puissance4.h */
+int VerificationColonnes(int** grille, int colonne, int joueur, int taille){
+	for (int i=0; i<taille-3; i++){
+		if(grille[i][colonne] == grille[i+1][colonne] && grille[i+1][colonne] == grille[i+2][colonne] && grille[i+2][colonne] == grille[i+3][colonne] && grille[i][colonne]!=-1){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/* same as Verificationdiag1 in puissance4.h */
+int Verificationdiag1(int** grille, int joueur, int taille){
+	for (int i=0; i < taille-3; i++){
+		for (int j=0; j < taille-3;j++){
+			if(grille[i][j] == grille[i+1][j+1] && grille[i+1][j+1] == grille[i+2][j+2] && grille[i+2][j+2] == grille[i+3][j+3] && grille[i][j]!=-1){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+/* same as Verificationdiag2 in puissance4.h*/
+int Verificationdiag2(int** grille, int joueur, int taille){
+	for (int i=0; i<taille-3; i++){
+		for (int j=0; j < taille-3;j++){
+			if(grille[i+3][j] == grille[i+2][j+1] && grille[i+2][j+1] == grille[i+1][j+2] && grille[i+1][j+2] == grille[i][j+3] && grille[i+3][j]!=-1){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+/* same as aGagne in puissance4.h */
+int aGagne(int** grille, int taille){
+	for (int i=1; i<=2; i++){
+		for (int j=0; j<taille; j++)
+	}
 }
